85311f97874fdc1963aad5c6f148cb728d0e1a84
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-03 16:08:05 +0100

    add: first 9 requettes

---
+++ query.sql
@@ -0,0 +1,18 @@
+INSERT INTO Customers (full_name,email,phone,registration_date)
+VALUES('Mohamed lfrah','lfrah@gmail.com','0712345678','2025-01-10 10:00:00');
+
+UPDATE Customers SET phone = '0600000000' WHERE full_name='Mohamed lfrah';
+
+SELECT * FROM Customers;
+
+SELECT full_name, email FROM Customers;
+
+SELECT * FROM Accounts;
+
+SELECT account_number FROM Accounts;
+
+SELECT * FROM Transactions;
+
+SELECT * FROM Accounts WHERE balance > 10000;
+
+SELECT * FROM Accounts WHERE balance <= 0;

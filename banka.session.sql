b72beff887e956da8f80472ec013b6d95293e71b
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-03 15:51:11 +0100

    add : data inserted : Advisors

--- banka.session.sql
+++ banka.session.sql
@@ -33,9 +33,13 @@ CREATE TABLE IF NOT EXISTS Transactions(
     account_id INT UNSIGNED,
     FOREIGN KEY (account_id) REFERENCES Accounts(id)
 );
-INSERT INTO Customers (full_name, email, phone, registration_date) VALUES
+INSERT INTO  Customers (full_name, email, phone, registration_date) VALUES
 ('Karim El Fassi', 'karim.fassi@gmail.com', '0610203040', '2025-02-01 09:20:00'),
 ('Samira Ouahid', 'samira.ouahid@gmail.com', '0621314151', '2025-02-02 10:10:00'),
 ('Youssef Haddad', 'youssef.haddad@gmail.com', '0632425262', '2025-02-03 14:30:00'),
 ('Lina Ajour', 'lina.ajour@gmail.com', '0643536373', '2025-02-04 11:45:00');
 
+INSERT INTO Advisors (full_name, email) VALUES
+('Nadia Expert', 'advisor.nadia@gmail.com'),
+('Rachid Finance', 'advisor.rachid@gmail.com'),
+('Imane Pro', 'advisor.imane@gmail.com');
